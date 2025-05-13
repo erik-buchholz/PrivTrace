4134bdec59e7148e02e43f1338a5b26f13d0e76e
Erik Buchholz <e.buchholz@unsw.edu.au>
2025-05-13 13:24:29 +1000

    Added some log messages.

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 """Evaluation Code."""
-
 import datetime
+import logging
 import multiprocessing
 from pathlib import Path
 from timeit import default_timer as timer
@@ -8,7 +8,6 @@ from timeit import default_timer as timer
 import pandas as pd
 
 import config.folder_and_file_names as fname
-# from tools.object_store import ObjectStore
 from config.parameter_carrier import ParameterCarrier
 from config.parameter_setter import ParSetter
 from data_preparation.data_preparer import DataPreparer
@@ -20,14 +19,23 @@ from tools.data_writer import DataWriter
 from tools.utils import printc
 
 # CONSTANTS
+DEBUG = True
 DATASETS = ["PORTO", "GEOLIFE"]
 N_FOLDS = 5
-N_TRAJS = 3000
+N_TRAJS = 3000  # Number of trajectories to generate for our evaluation
 EPSILON = 10.0
 epsilons = [10.0]
 
+# Set up log
+logging.basicConfig(
+    format="%(asctime)s - [%(levelname)s]: %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+log = logging.getLogger(__name__)
 
-def run(dataset: str, fold: int, epsilon: float = EPSILON):
+
+def run(dataset: str, fold: int, epsilon: float = EPSILON) -> None:
     # Copy of main.py
 
     input_filename = f"{dataset}_{fold}.dat"
@@ -39,21 +47,40 @@ def run(dataset: str, fold: int, epsilon: float = EPSILON):
     writer = DataWriter()
     print('begin all')
     print(datetime.datetime.now())
-    par = ParSetter().set_up_args(dataset_file_name=input_filename, output_file_name=output_filename,
-                                  epsilon=epsilon, out_size=N_TRAJS)
+    par = ParSetter().set_up_args(
+        dataset_file_name=input_filename,  # Input File
+        output_file_name=output_filename,  # Output File
+        epsilon=epsilon,  # Final Epsilon (e1 + e2 + e3)
+        out_size=N_TRAJS  # Number of trajectories to generate for our evaluation
+    )
+
     pc = ParameterCarrier(par)
+
+    # Load Data
     data_preparer = DataPreparer(par)
     trajectory_set = data_preparer.get_trajectory_set()
+    log.info("Preparing data finished")
+
+    # Step 1: Discretisation
     disdata1 = DisData(pc)
     grid = disdata1.get_discrete_data(trajectory_set)
+    log.info("Discretization finished")
+
+    # Step 2: Learn Markov Models
     mb1 = ModelBuilder(pc)
     mo1 = mb1.build_model(grid, trajectory_set)
+    log.info("Markov model building finished")
     mb1 = ModelBuilder(pc)
     mo1 = mb1.filter_model(trajectory_set, grid, mo1)
+
+    # Step 3: Generate Trajectories
+    log.info("Markov model filtering finished")
     sg1 = StateGeneration(pc)
     st_tra_list = sg1.generate_tra(mo1)
+    log.info("State trajectory generation finished")
     rlt1 = RealLocationTranslator(pc)
     real_tra_list = rlt1.translate_trajectories(grid, st_tra_list)
+    log.info("Real trajectory generation finished")
     writer.save_trajectory_data_in_list_to_file(real_tra_list, par['output'])
     print('end all')
     print(datetime.datetime.now())
@@ -71,24 +98,28 @@ def run(dataset: str, fold: int, epsilon: float = EPSILON):
         df.to_csv(filepath, mode='w', header=True, index=False)
 
 
-
 if __name__ == "__main__":
-    processes = []
-
-    for dataset in DATASETS:
-        for fold in range(1, N_FOLDS+1):
-            for epsilon in epsilons:
-                p = multiprocessing.Process(
-                    target=run,
-                    kwargs={
-                        "dataset": dataset,
-                        "fold": fold,
-                        "epsilon": epsilon,
-                    },
-                )
-                processes.append(p)
-                print(f"Starting process for {dataset} fold {fold} with epsilon {epsilon}.")
-                p.start()
+    if DEBUG:
+        # Set logging to debug level
+        log.setLevel(logging.DEBUG)
+        # Only run geolife dataset with fold 1 and epsilon 10.0
+        run(dataset="GEOLIFE", fold=1, epsilon=10.0)
+    else:
+        processes = []
+        for dataset in DATASETS:
+            for fold in range(1, N_FOLDS + 1):
+                for epsilon in epsilons:
+                    p = multiprocessing.Process(
+                        target=run,
+                        kwargs={
+                            "dataset": dataset,
+                            "fold": fold,
+                            "epsilon": epsilon,
+                        },
+                    )
+                    processes.append(p)
+                    print(f"Starting process for {dataset} fold {fold} with epsilon {epsilon}.")
+                    p.start()
 
     for p in processes:
         p.join()
